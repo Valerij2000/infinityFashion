6d9637fe9671ff933776d1d667aefc2994f62d8e
Valerij Shumkov <valery.shumkov@mail.ru>
2021-09-10 14:23:13 +0300

    create new links pages and add photo

--- header.php
+++ header.php
@@ -5,9 +5,22 @@
 			<div id='cssmenu' class="align-center">
 				<ul>
 				   <li class="active"><a href='index.php'><span>главная</span></a></li>
-				   <li><a href='about.php'><span>о себе</span></a></li>
+					 <li><a href='archive.php'><span>купальники</span></a></li>
+				   <li><a href='kartigans.php'><span>картиганы</span></a></li>
 				   <li><a href='gallery.php'><span>галерея</span></a></li>
-				   <li><a href='archive.php'><span>купальники</span></a></li>
+					 <li class=' has-sub'><a href='archive.html'><span>каталог</span></a>
+					  <ul>
+						 <li class='has-sub'><a href='#'><span>Плавки</span></a>
+							<ul>
+						   	<li><a href='#'><span>Резиновые шапки</span></a></li>
+						   	<li class='last'><a href='#'><span>Парео</span></a></li>
+						  	<li class='last'><a href='#'><span>Туники</span></a></li>
+							</ul>
+						 </li>
+						 <li class='has-sub'><a href='#'><span>Куртки</span></a></li>
+						 <li class='has-sub'><a href='#'><span>Пуховики	</span></a></li>
+					  </ul>
+				   </li>
 				   <li class='last'><a href='contact-page.php'><span>контакты</span></a></li>
 				</ul>
 			</div>
