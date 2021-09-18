38af4e55728cd86938747649b87e4f8543b7e43a
Valerij Shumkov <valery.shumkov@mail.ru>
2021-09-18 23:25:13 +0300

    create exit krestic in mobile menu

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 ( function( $ ) {
 $( document ).ready(function() {
-$('#cssmenu').prepend('<div id="menu-button">Меню</div>');
+$('#cssmenu').prepend('<div id="menu-button" class="mobile-menu">Меню</div>');
 	$('#cssmenu #menu-button').on('click', function(){
 		var menu = $(this).next('ul');
 		if (menu.hasClass('open')) {
 			menu.removeClass('open');
+			$(".mobile-menu").attr('id', 'menu-button');
+			$(".mobile-menu").removeClass("menu-button__exit");
 		}
 		else {
+			$(".mobile-menu").removeAttr("id");
+			$(".mobile-menu").addClass("menu-button__exit");
 			menu.addClass('open');
 		}
 	});
