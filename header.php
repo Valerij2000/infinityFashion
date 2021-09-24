b0f40fd5fca72757e7565f8064e426c611170d43
Valerij Shumkov <valery.shumkov@mail.ru>
2021-09-24 22:08:12 +0300

    add new nav catalog

--- header.php
+++ header.php
@@ -5,20 +5,25 @@
 			<nav id='cssmenu' class="align-center">
 				<ul>
 				   <li class="active"><a href='index.php'><span>главная</span></a></li>
-					 <li><a href='archive.php'><span>купальники</span></a></li>
+					 <li class='has-sub'><a><span>каталог купальников</span></a>
+					 	<ul>
+							 <li class='has-sub'><a href='archive.php'><span>Dea Fiori 2022 г.</span></a></li>
+							 <li class='has-sub'><a href='images/swimsuit/self_katalog_swim.pdf' target="images/swimsuit/self_katalog_swim.pdf"><span>Self 2022 г.</span></a></li>
+							 <li class='has-sub'><a href='images/swimsuit/self_katalog_swim_continue.pdf' target="images/swimsuit/self_katalog_swim.pdf"><span>Self под заказ</span></a></li>
+							 <li class='has-sub'><a href='sport-swim.php'><span>Спортивные купальники	</span></a></li>
+							 <li class='has-sub'><a href='images/swimsuit/self_plavki.pdf' target="images/swimsuit/self_plavki.pdf"><span>Плавки</span></a>
+ 								<ul>
+ 							   	<li><a href='#'><span>Резиновые шапки</span></a></li>
+ 							   	<li class='last'><a href='#'><span>Парео</span></a></li>
+ 							  	<li class='last'><a href='#'><span>Туники</span></a></li>
+ 								</ul>
+ 						 	</li>
+					 	</ul>
 				   <li><a href='kartigans.php'><span>картиганы</span></a></li>
 					 <li class='has-sub'><a><span>каталог</span></a>
 					  <ul>
-							<li class='has-sub'><a href='sport-swim.php'><span>Спортивные купальники	</span></a></li>
-						 	<li class='has-sub'><a href='#'><span>Плавки</span></a>
-								<ul>
-							   	<li><a href='#'><span>Резиновые шапки</span></a></li>
-							   	<li class='last'><a href='#'><span>Парео</span></a></li>
-							  	<li class='last'><a href='#'><span>Туники</span></a></li>
-								</ul>
-						 	</li>
 						 <li class='has-sub'><a href='#'><span>Куртки</span></a></li>
-						 <li class='has-sub'><a href='down-jackets.php'><span>Пуховики	</span></a></li>
+						 <li class='has-sub'><a href='down-jackets.php'><span>Пуховики</span></a></li>
 					  </ul>
 				   </li>
 				   <li class='last'><a href='contact-page.php'><span>контакты</span></a></li>
